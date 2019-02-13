ae18238d008957510a2ad56bbc278a7a715661c6
Yash Lamba <yashlamba2000@gmail.com>
2019-02-13 22:06:19 +0530

    Added the basic crawler

---
+++ GSOC.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+class GsocSpider(scrapy.Spider):
+    name = 'GSOC'
+    allowed_domains = ['summerofcode.withgoogle.com/archive/2018/organizations/']
+    start_urls = ['https://summerofcode.withgoogle.com/archive/2018/organizations/']
+    
+    def parse(self, response):
+        org_links = response.css('a.organization-card__link::attr(href)').extract()
+        for org in org_links:
+            link = response.urljoin(org)
+            yield scrapy.Request(url = link, callback = self.parseorg, dont_filter = True)
+
+    def parseorg(self, response):
+        yield {
+            'name': response.css('h3.banner__title::text').extract_first(),
+            'website': response.css('a.org__link::attr(href)').extract_first(),
+        }
+
